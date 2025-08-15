d9970a9f612cfcdcd64284775c5271d8d39ed31d
hascon <maruri.chibi211@gmail.com>
2025-08-15 09:22:17 +0900

    next.config.jsでバカをやってたので修正。

--- clientApp/next.config.js
+++ clientApp/next.config.js
@@ -123,7 +123,7 @@ const nextConfig = {
 
   // 共通設定
   images: {
-    domains: ['localhost', 'your-domain.com'],
+    domains: ['localhost', process.env.NEXT_PUBLIC_DOMAIN.split('//')[1]],
     formats: ['image/webp', 'image/avif'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
